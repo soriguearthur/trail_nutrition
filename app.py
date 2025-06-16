e56e0a44d842d73d5d3babe93e7aa35c208bd168
soriguearthur <94493649+soriguearthur@users.noreply.github.com>
2025-06-16 15:19:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,11 @@ if predict_time:
 
         except Exception as e:
             st.write("course non présente dans la base de données -> estimation via machine learning")
-
+            estimation = get_prediction(name,utmb_index,predicted_distance,predicted_dplus)
+            st.session_state['estimated_time'] = estimation
+            st.session_state['course_found'] = name
+            st.session_state['distance_found'] = predicted_distance
+            st.session_state['denivele_found'] = predicted_dplus
 
 # Affichage de la valeur persistante même sans re-estimation
 st.write(f"{st.session_state['course_found']} - {st.session_state['distance_found']}km - {st.session_state['denivele_found']}D+ Temps estimé actuel : **{formater_duree(st.session_state['estimated_time'])} ({st.session_state['estimated_time']:.2f}h)**")
